5363f99f8a62c80ca9781dec4f9b3be479aabbcf
Maxime Bury <maxime.bury@gmail.com>
2016-04-18 12:42:36 -0400

    Make install a bit more robust

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 set -ex
 
-git clone --recursive https://github.com/Bowbaq/osx-bootstrap.git "$HOME/.osx-bootstrap"
+if [ ! -d "$HOME/.osx-bootstrap" ]; then
+  git clone --recursive https://github.com/Bowbaq/osx-bootstrap.git "$HOME/.osx-bootstrap"
+else
+  cd "$HOME/.osx-bootstrap"; git fetch; git reset --hard origin/master
+fi
+
 cd "$HOME/.osx-bootstrap"
 bash bootstrap.sh
